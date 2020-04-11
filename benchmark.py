d7e68c387bc80952a7689e2c773353eb2c549f19
rcy <rcycyzh@163.com>
2020-04-12 00:12:35 +0800

    Fix bug

--- benchmark.py
+++ benchmark.py
@@ -26,7 +26,7 @@ def main(file_in, file_answer):
     line_count, line_correct = len(results), 0
     for result, ans in zip(results, answers):
         char_count += len(result)
-        char_correct += reduce(lambda a, b: a + (b[0] == b[1]), zip(results, ans), 0)
+        char_correct += reduce(lambda a, b: a + (b[0] == b[1]), zip(result, ans), 0)
         line_correct += result == ans
     print(model.smooth_1, model.smooth_2, char_correct / char_count, line_correct / line_count)
 
